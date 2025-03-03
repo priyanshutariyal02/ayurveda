947c8205f52564636ebd6b0f1311743aebda46d8
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2025-03-03 20:40:06 +0530

    fix: logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from "next/server";
 
-export async function POST(_req: Request, res: NextResponse) {
+export async function POST(_req: Request) {
   try {
-    res.cookies.set("token", "", { expires: new Date(0) });
-    return NextResponse.json(
+    const response = NextResponse.json(
       { message: "Logged out successfully!" },
       { status: 200 }
     );
+
+    response.cookies.set("token", "", { expires: new Date(0) });
+
+    return response;
   } catch (error: any) {
     return NextResponse.json(
-      { message: "Error logging out!", error },
+      { message: "Error logging out!", error: error.message },
       { status: 500 }
     );
   }
--- src/app/api/auth/myDetails/route.ts
+++ src/app/api/auth/myDetails/route.ts
@@ -1,18 +1,20 @@
-import { NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
+import { User } from "@/models/user.models";
 
-export async function POST(_req: Request) {
+export async function GET(req: Request) {
   try {
-    const response = NextResponse.json(
-      { message: "Logged out successfully!" },
-      { status: 200 }
-    );
-
-    response.cookies.set("token", "", { expires: new Date(0) });
-
-    return response;
+    const userId = req.headers.get("userId");
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json(
+        { message: "Cannot find user" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ user }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json(
-      { message: "Error logging out!", error: error.message },
+      { message: "Error getting user details!", error },
       { status: 500 }
     );
   }
